900f54bf786e69a832ed75472d94d1bd89d53a43
raguAGC <raguram.barathan@agcapps.com>
2023-02-10 13:37:11 +0000

    Sync Error

--- Rules/SmartInspectionsErrorArchive_SyncErrorsCount.js
+++ Rules/SmartInspectionsErrorArchive_SyncErrorsCount.js
@@ -1,10 +1,12 @@
 export default function SmartInspectionsErrorArchive_SyncErrorsCount(context) {
 	return context.count('/SmartInspections/Services/SAM.service', 'ErrorArchive', '').then(errorCount => {
-		if (errorCount > 0) {
-			return errorCount;
-		} else {
-			return '';
-		}
+		var sTitle = "SI Errors (" + errorCount + ")";
+		return sTitle;
+		// if (errorCount > 0) {
+		// 	return errorCount;
+		// } else {
+		// 	return '';
+		// }
 	}).catch(() => {
 		return Promise.resolve();
 	});
