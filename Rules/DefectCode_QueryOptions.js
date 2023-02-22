82e3703d46f55468326d21a1295b4d02f79af91d
raguAGC <raguram.barathan@agcapps.com>
2023-02-22 07:26:00 +0000

    Defect Code Query Filter

--- Rules/DefectCode_QueryOptions.js
+++ Rules/DefectCode_QueryOptions.js
@@ -1,5 +1,38 @@
-export default function DefectCode_QueryOptions(sectionedTableProxy) {
-	var technicalObject = sectionedTableProxy.binding.TechnicalObject;
-	var queryOptions = "$filter=TechnicalObject eq '" + technicalObject + "'&$orderby=DefCode";
-	return queryOptions;
+// export default function DefectCode_QueryOptions(sectionedTableProxy) {
+// 	var technicalObject = sectionedTableProxy.binding.TechnicalObject;
+// 	var queryOptions = "$filter=TechnicalObject eq '" + technicalObject + "'&$orderby=DefCode";
+// 	return queryOptions;
+// }
+//Start of AM001
+export default function DefectCode_QueryOptions(clientAPI) {
+	//Tech Object page
+	var listPageClientData = clientAPI.evaluateTargetPath("#Page:TechnicalObjectDetailsList/#ClientData");
+	var oTechbinding = listPageClientData.NavigatedTechObject;
+	// Catalog Profile at Tech object level
+	var sCatalogProfile = oTechbinding.CatalogProfile;
+	//Build Query string
+	var sQuery = "$filter=CatalogProfile eq '" + sCatalogProfile + "'";
+	//Read the Catalog
+	return clientAPI.read('/SmartInspections/Services/SAM.service', 'CatalogProfileDetailsSet', [],
+		sQuery).then(function (results) {
+			//Build the Code Groups
+			var aCodeGroups = [];
+			if (results && results.length > 0) {
+				results.forEach(function (value) {
+					aCodeGroups.push(value.DefectCodeGroup);
+				});
+			} //if 
+			//prepare  Filter Lines
+			var aCodeGroupsFlines = [];
+			for (var i = 0; i < aCodeGroups.length; i++) {
+				var sLine = "DefCodeGroup eq '" + aCodeGroups[i] + "'";
+				aCodeGroupsFlines.push(sLine);
+			}
+			//Build Final Filter String
+			var sGroups = aCodeGroupsFlines.join(" or ");
+			var sFilterQuery = "$filter=(" + sGroups + ")&$orderby=DefCode";
+			return sFilterQuery;
+		});//then
+
 }
+//End of AM001
