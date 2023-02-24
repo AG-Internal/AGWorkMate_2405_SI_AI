e17cd98ac7e823c641abe053d6bd8d3682b06ce4
raguAGC <raguram.barathan@agcapps.com>
2023-02-24 16:22:17 +0000

    Last Inspection Date

--- Rules/InspectionCharacteristicDetailsPage_PreInsp.js
+++ Rules/InspectionCharacteristicDetailsPage_PreInsp.js
@@ -1,12 +1,31 @@
+/*--------------------------------------------------------------------*
+* Change Tag    :  CR005
+*--------------------------------------------------------------------*/
 import libVal from './Common/ValidationLibrary';
 
 export default function InspectionCharacteristicDetailsPage_PreInsp(context) {
 	var binding = context.binding;
 
+	// if (!libVal.evalIsEmpty(binding.LastInspType)) {
+	// 	var lastInspTypeDesc = "PREVIOUS - " + binding.LastInspTypeDesc;
+	// 	return lastInspTypeDesc;
+	// } else {
+	// 	return '';
+	// }
+	//B.O.A for CR005
+	var sFinalValue = "";
 	if (!libVal.evalIsEmpty(binding.LastInspType)) {
-		var lastInspTypeDesc = "PREVIOUS - " + binding.LastInspTypeDesc;
-		return lastInspTypeDesc;
-	} else {
-		return '';
+		sFinalValue = binding.LastInspTypeDesc;
 	}
+	if (binding.LastInspDateChar) {
+		sFinalValue = sFinalValue + ", " + binding.LastInspDateChar;
+	}
+
+	if (sFinalValue) {
+		sFinalValue = "PREVIOUS - " + sFinalValue;
+	}
+
+	return sFinalValue;
+
+	//B.O.A for CR005
 }
